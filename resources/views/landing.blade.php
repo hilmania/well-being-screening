a206683edd8cedc00c7349ad2490be6a092a34c7
Hilman Ibnu <hia.develop@gmail.com>
2025-10-15 05:49:42 +0700

    chore: remove section dipercaya on landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -151,8 +151,8 @@
           <!-- /column -->
         </div>
         <!-- /.row -->
-        <p class="text-center !mb-8">Dipercaya oleh lebih dari 2K+ klien di seluruh Indonesia</p>
-        <div class="flex flex-wrap mx-[-15px] row-cols-4 row-cols-md-4 row-cols-lg-7 row-cols-xl-7 !mt-[-50px] !mb-2 items-center justify-center">
+        {{-- <p class="text-center !mb-8">Dipercaya oleh lebih dari 2K+ klien di seluruh Indonesia</p> --}}
+        {{-- <div class="flex flex-wrap mx-[-15px] row-cols-4 row-cols-md-4 row-cols-lg-7 row-cols-xl-7 !mt-[-50px] !mb-2 items-center justify-center">
           <div class="w-1/4 xl:w-[14.2857142857%] lg:w-[14.2857142857%] md:w-1/4 flex-[0_0_auto] !px-[15px] !mt-[50px] max-w-full">
             <img  class="md:!px-[0.75rem] lg:!px-0 xl:!px-[0.5rem] xxl:!px-[1.25rem]" src="./assets/img/brands/c1.png" alt="image">
           </div>
@@ -180,7 +180,7 @@
           <div class="w-1/4 xl:w-[14.2857142857%] lg:w-[14.2857142857%] md:w-1/4 flex-[0_0_auto] !px-[15px] !mt-[50px] max-w-full">
             <img  class="md:!px-[0.75rem] lg:!px-0 xl:!px-[0.5rem] xxl:!px-[1.25rem]" src="./assets/img/brands/c7.png" alt="image">
           </div>
-          <!--/column -->
+          <!--/column --> --}}
         </div>
         <!-- /.row -->
       </div>
