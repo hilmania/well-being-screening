5f3eaf459e4d5b7ff7c199bf3fbf32c53009a284
Hilman Ibnu <hia.develop@gmail.com>
2025-10-14 21:51:13 +0700

    chore: revamp landing page remove unnecessary section

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -233,70 +233,6 @@
       <!-- /.container -->
     </section>
     <!-- /section -->
-    <section class="wrapper !bg-[#ffffff]">
-      <div class="container pb-[4.5rem] xl:pb-28 lg:pb-28 md:pb-28">
-        <div class="flex flex-wrap mx-[-15px] md:mx-[-20px] lg:mx-[-20px] xl:mx-[-35px] !mt-[-50px] items-center">
-          <div class="xl:w-6/12 lg:w-6/12 w-full flex-[0_0_auto] !px-[15px] md:!px-[20px] lg:!px-[20px] xl:!px-[35px] !mt-[50px] max-w-full xl:!order-2 lg:!order-2">
-            <div class="card !shadow-[0_0.25rem_1.75rem_rgba(30,34,40,0.07)] lg:!mr-6 xl:!mr-6">
-              <div class="card-body p-6">
-                <div class="flex flex-row">
-                  <div>
-                    <span class="icon btn btn-circle btn-lg btn-soft-sky pointer-events-none !mr-4 xl:!text-[1.3rem] !w-12 !h-12 !text-[calc(1.255rem_+_0.06vw)] inline-flex items-center justify-center leading-none !p-0 !rounded-[100%]"><span class="number table-cell text-center align-middle text-[1.1rem] font-bold m-[0_auto]">01</span></span>
-                  </div>
-                  <!-- <div>
-                    <h4 class="!mb-1">Collect Ideas</h4>
-                    <p class="!mb-0">Nulla vitae elit libero pharetra augue dapibus.</p>
-                  </div> -->
-                </div>
-              </div>
-              <!--/.card-body -->
-            </div>
-            <!--/.card -->
-            <div class="card !shadow-[0_0.25rem_1.75rem_rgba(30,34,40,0.07)] lg:!ml-16 xl:!ml-16 !mt-6">
-              <div class="card-body p-6">
-                <div class="flex flex-row">
-                  <div>
-                    <span class="icon btn btn-circle btn-lg btn-soft-sky pointer-events-none !mr-4 xl:!text-[1.3rem] !w-12 !h-12 !text-[calc(1.255rem_+_0.06vw)] inline-flex items-center justify-center leading-none !p-0 !rounded-[100%]"><span class="number table-cell text-center align-middle text-[1.1rem] font-bold m-[0_auto]">02</span></span>
-                  </div>
-                  <!-- <div>
-                    <h4 class="!mb-1">Data Analysis</h4>
-                    <p class="!mb-0">Vivamus sagittis lacus vel augue laoreet.</p>
-                  </div> -->
-                </div>
-              </div>
-              <!--/.card-body -->
-            </div>
-            <!--/.card -->
-            <div class="card !shadow-[0_0.25rem_1.75rem_rgba(30,34,40,0.07)] lg:mx-6 xl:mx-6 !mt-6">
-              <div class="card-body p-6">
-                <div class="flex flex-row">
-                  <div>
-                    <span class="icon btn btn-circle btn-lg btn-soft-sky pointer-events-none !mr-4 xl:!text-[1.3rem] !w-12 !h-12 !text-[calc(1.255rem_+_0.06vw)] inline-flex items-center justify-center leading-none !p-0 !rounded-[100%]"><span class="number table-cell text-center align-middle text-[1.1rem] font-bold m-[0_auto]">03</span></span>
-                  </div>
-                  <!-- <div>
-                    <h4 class="!mb-1">Finalize Product</h4>
-                    <p class="!mb-0">Cras mattis consectetur purus sit amet.</p>
-                  </div> -->
-                </div>
-              </div>
-              <!--/.card-body -->
-            </div>
-            <!--/.card -->
-          </div>
-          <!--/column -->
-          <!-- <div class="xl:w-6/12 lg:w-6/12 w-full flex-[0_0_auto] !px-[15px] md:!px-[20px] lg:!px-[20px] xl:!px-[35px] !mt-[50px] max-w-full">
-            <h2 class="!text-[0.8rem] uppercase !text-[#5eb9f0] !mb-3 !leading-[1.35] !tracking-[0.02rem]">Our Strategy</h2>
-            <h3 class="xl:!text-[2.1rem] !text-[calc(1.335rem_+_1.02vw)] !leading-[1.2] !mb-4">Here are 3 working steps to organize our projects.</h3>
-            <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis.</p>
-            <p class="!mb-6">Nullam id dolor id nibh ultricies vehicula ut id elit. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur.</p>
-            <a href="#" class="btn btn-sky !text-white !bg-[#5eb9f0] border-[#5eb9f0] hover:text-white hover:!bg-[#5eb9f0] hover:!border-[#5eb9f0] focus:shadow-[rgba(88,167,216,1)] active:text-white active:!bg-[#5eb9f0] active:border-[#5eb9f0] disabled:text-white disabled:!bg-[#5eb9f0] disabled:border-[#5eb9f0]  !rounded-[50rem] !mb-0 hover:translate-y-[-0.15rem] hover:shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.15)]">Learn More</a>
-          </div> -->
-          <!--/column -->
-        </div>
-        <!--/.row -->
-      </div>
-      <!-- /.container -->
-    </section>
     <!-- /section -->
     <section class="wrapper !bg-[#ffffff]">
       <div class="container !py-[4.5rem] xl:!py-24 lg:!py-24 md:!py-24">
@@ -420,7 +356,7 @@
       <div class="flex flex-wrap mx-[-15px] !mt-[-30px] xl:!mt-0 lg:!mt-0">
         <div class="md:w-4/12 xl:w-3/12 lg:w-3/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!mt-0 lg:!mt-0 !mt-[30px]">
           <div class="widget !text-[#cacaca]">
-            <img class="!mb-4" src="./assets/img/logo-light.png" srcset="./assets/img/logo-light@2x.png 2x" alt="image">
+            {{-- <img class="!mb-4" src="./assets/img/logo-light.png" srcset="./assets/img/logo-light@2x.png 2x" alt="image">
             <p class="!mb-4">© 2024 Sandbox. <br class="hidden xl:block lg:block !text-[#cacaca]">All rights reserved.</p>
             <nav class="nav social social-white">
               <a class="!text-[#cacaca] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]" href="#"><i class="uil uil-twitter before:content-['\ed59'] !text-white text-[1rem]"></i></a>
@@ -428,22 +364,22 @@
               <a class="!text-[#cacaca] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]" href="#"><i class="uil uil-dribbble before:content-['\eaa2'] !text-white text-[1rem]"></i></a>
               <a class="!text-[#cacaca] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]" href="#"><i class="uil uil-instagram before:content-['\eb9c'] !text-white text-[1rem]"></i></a>
               <a class="!text-[#cacaca] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 motion-reduce:transition-none hover:translate-y-[-0.15rem] m-[0_.7rem_0_0]" href="#"><i class="uil uil-youtube before:content-['\edb5'] !text-white text-[1rem]"></i></a>
-            </nav>
+            </nav> --}}
             <!-- /.social -->
           </div>
           <!-- /.widget -->
         </div>
         <!-- /column -->
         <div class="md:w-4/12 xl:w-3/12 lg:w-3/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!mt-0 lg:!mt-0 !mt-[30px]">
-          <div class="widget !text-[#cacaca]">
+          {{-- <div class="widget !text-[#cacaca]">
             <h4 class="widget-title !text-white !mb-3">Get in Touch</h4>
             <address class="xl:!pr-20 xxl:!pr-28 not-italic !leading-[inherit] block !mb-4">Moonshine St. 14/05 Light City, London, United Kingdom</address>
             <a class="!text-[#cacaca] hover:!text-[#5eb9f0]" href="mailto:first.last@email.com">info@email.com</a><br> 00 (123) 456 78 90
-          </div>
+          </div> --}}
           <!-- /.widget -->
         </div>
         <!-- /column -->
-        <div class="md:w-4/12 xl:w-3/12 lg:w-3/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!mt-0 lg:!mt-0 !mt-[30px]">
+        {{-- <div class="md:w-4/12 xl:w-3/12 lg:w-3/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!mt-0 lg:!mt-0 !mt-[30px]">
           <div class="widget !text-[#cacaca]">
             <h4 class="widget-title !text-white !mb-3">Learn More</h4>
             <ul class="pl-0 list-none   !mb-0">
@@ -455,15 +391,15 @@
             </ul>
           </div>
           <!-- /.widget -->
-        </div>
+        </div> --}}
         <!-- /column -->
         <div class="md:w-full xl:w-3/12 lg:w-3/12 w-full flex-[0_0_auto] !px-[15px] max-w-full xl:!mt-0 lg:!mt-0 !mt-[30px]">
           <div class="widget !text-[#cacaca]">
-            <h4 class="widget-title !text-white !mb-3">Our Newsletter</h4>
-            <p class="!mb-5">Subscribe to our newsletter to get our news & deals delivered to you.</p>
+            {{-- <h4 class="widget-title !text-white !mb-3">Our Newsletter</h4>
+            <p class="!mb-5">Subscribe to our newsletter to get our news & deals delivered to you.</p> --}}
             <div class="newsletter-wrapper">
               <!-- Begin Mailchimp Signup Form -->
-              <div id="mc_embed_signup2">
+              {{-- <div id="mc_embed_signup2">
                 <form action="https://elemisfreebies.us20.list-manage.com/subscribe/post?u=aa4947f70a475ce162057838d&amp;id=b49ef47a9a" method="post" id="mc-embedded-subscribe-form2" name="mc-embedded-subscribe-form" class="validate dark-fields" target="_blank" novalidate>
                   <div id="mc_embed_signup_scroll2">
                     <div class="!text-left input-group form-floating !relative flex flex-wrap items-stretch w-full">
@@ -479,7 +415,7 @@
                     <div class="clear"></div>
                   </div>
                 </form>
-              </div>
+              </div> --}}
               <!--End mc_embed_signup-->
             </div>
             <!-- /.newsletter-wrapper -->
