c4aab6790fee72def86ec25cfab24ac66734f165
FakhriadiRasyaad <161245848+FakhriadiRasyaad@users.noreply.github.com>
2025-10-14 17:25:01 +0700

    deleted the our strategy section

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -243,10 +243,10 @@
                   <div>
                     <span class="icon btn btn-circle btn-lg btn-soft-sky pointer-events-none !mr-4 xl:!text-[1.3rem] !w-12 !h-12 !text-[calc(1.255rem_+_0.06vw)] inline-flex items-center justify-center leading-none !p-0 !rounded-[100%]"><span class="number table-cell text-center align-middle text-[1.1rem] font-bold m-[0_auto]">01</span></span>
                   </div>
-                  <div>
+                  <!-- <div>
                     <h4 class="!mb-1">Collect Ideas</h4>
                     <p class="!mb-0">Nulla vitae elit libero pharetra augue dapibus.</p>
-                  </div>
+                  </div> -->
                 </div>
               </div>
               <!--/.card-body -->
@@ -258,10 +258,10 @@
                   <div>
                     <span class="icon btn btn-circle btn-lg btn-soft-sky pointer-events-none !mr-4 xl:!text-[1.3rem] !w-12 !h-12 !text-[calc(1.255rem_+_0.06vw)] inline-flex items-center justify-center leading-none !p-0 !rounded-[100%]"><span class="number table-cell text-center align-middle text-[1.1rem] font-bold m-[0_auto]">02</span></span>
                   </div>
-                  <div>
+                  <!-- <div>
                     <h4 class="!mb-1">Data Analysis</h4>
                     <p class="!mb-0">Vivamus sagittis lacus vel augue laoreet.</p>
-                  </div>
+                  </div> -->
                 </div>
               </div>
               <!--/.card-body -->
@@ -273,10 +273,10 @@
                   <div>
                     <span class="icon btn btn-circle btn-lg btn-soft-sky pointer-events-none !mr-4 xl:!text-[1.3rem] !w-12 !h-12 !text-[calc(1.255rem_+_0.06vw)] inline-flex items-center justify-center leading-none !p-0 !rounded-[100%]"><span class="number table-cell text-center align-middle text-[1.1rem] font-bold m-[0_auto]">03</span></span>
                   </div>
-                  <div>
+                  <!-- <div>
                     <h4 class="!mb-1">Finalize Product</h4>
                     <p class="!mb-0">Cras mattis consectetur purus sit amet.</p>
-                  </div>
+                  </div> -->
                 </div>
               </div>
               <!--/.card-body -->
@@ -284,13 +284,13 @@
             <!--/.card -->
           </div>
           <!--/column -->
-          <div class="xl:w-6/12 lg:w-6/12 w-full flex-[0_0_auto] !px-[15px] md:!px-[20px] lg:!px-[20px] xl:!px-[35px] !mt-[50px] max-w-full">
+          <!-- <div class="xl:w-6/12 lg:w-6/12 w-full flex-[0_0_auto] !px-[15px] md:!px-[20px] lg:!px-[20px] xl:!px-[35px] !mt-[50px] max-w-full">
             <h2 class="!text-[0.8rem] uppercase !text-[#5eb9f0] !mb-3 !leading-[1.35] !tracking-[0.02rem]">Our Strategy</h2>
             <h3 class="xl:!text-[2.1rem] !text-[calc(1.335rem_+_1.02vw)] !leading-[1.2] !mb-4">Here are 3 working steps to organize our projects.</h3>
             <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis.</p>
             <p class="!mb-6">Nullam id dolor id nibh ultricies vehicula ut id elit. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur.</p>
             <a href="#" class="btn btn-sky !text-white !bg-[#5eb9f0] border-[#5eb9f0] hover:text-white hover:!bg-[#5eb9f0] hover:!border-[#5eb9f0] focus:shadow-[rgba(88,167,216,1)] active:text-white active:!bg-[#5eb9f0] active:border-[#5eb9f0] disabled:text-white disabled:!bg-[#5eb9f0] disabled:border-[#5eb9f0]  !rounded-[50rem] !mb-0 hover:translate-y-[-0.15rem] hover:shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.15)]">Learn More</a>
-          </div>
+          </div> -->
           <!--/column -->
         </div>
         <!--/.row -->
